7082cc32563c2043030127d265eb330d2023e03e
TheBoy <theboy5140@gmail.com>
2017-02-07 15:34:21 +0800

    add controller

---
+++ framework/Controller.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * PROJECT: Kungfu
+ * AUTHOR :　TheBoy <theboy5140@gmail.com>
+ * FILE : Controller.php
+ * DATE : 2017/2/7 15:30
+ */
+
+namespace  TheBoy\Kungfu;
+
+class Controller
+{
+sfsfd
+}
