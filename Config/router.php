98da4d64a148fafbbdd198dea910250c9e173013
TheBoy <theboy5140@gmail.com>
2017-02-24 16:02:11 +0800

    add routers

--- Config/router.php
+++ Config/router.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * PROJECT: Kungfu
- * AUTHOR :　TheBoy <theboy5140@gmail.com>
- * FILE : router.php
- * DATE : 2017/2/9 16:31
- */
 
 return [
     "user" => ["url" => "/user", "class" => Application\Controller\Index::class],
